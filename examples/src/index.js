9c46e0225df2e0fba75a2a7fef5325140ee570e6
ygazez <yagmurgazezoglu@gmail.com>
2022-04-27 14:29:48 +0300

    first commit

---
+++ examples/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const date = new Date(12,1,1,20);
+const currenttime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+   color:""
+}
+if(currenttime<12){
+   greeting = "GOOD MORNİNG";
+   customStyle.color = "green";
+}
+else if(currenttime<18){
+   greeting = "GOOD AFTERNOON";
+   customStyle.color = "blue";
+}
+else {
+   greeting="GOOD NİGHT";
+   customStyle.color = "pink";
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <div>
+
+<h1 className='header' style = {customStyle} >{greeting} </h1>
+
+ </div>,
+    
+
+);
+
